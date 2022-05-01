5fe3dbde2bed9011c4cadb04697c4755d4b69aa1
Marco <macdev.gmail@gmail.com>
2022-04-30 20:16:51 -0500

    add a request with status 200 which returns a list of explorers

---
+++ app.js
@@ -0,0 +1,24 @@
+// Usando objeto express
+const express = require('express')
+// App de Express
+const app = express()
+app.use(express.json()) // Indicamos que usaremos JSON
+// Puerto en que vamos a ver nuestra app: localhost:3000
+const port = 3000
+
+// Con esto inicializamos esta app
+app.listen(port, () => {
+ console.log(`Example app listening on port ${port}`)
+})
+
+// HTTP METHODS
+
+app.get('/v1/explorers', (req, res) => {
+  console.log(`Api Explorers GET ALL requests ${new Date()}`)
+  const explorer1 = {id: 1,  name: "Marco1"}
+  const explorer2 = {id: 2,  name: "Marco2"}
+  const explorer3 = {id: 3,  name: "Marco3"}
+  const explorer4 = {id: 4,  name: "Marco4"}
+  const explorers = [explorer1, explorer2, explorer3, explorer4]
+  res.status(200).json(explorers)
+})
