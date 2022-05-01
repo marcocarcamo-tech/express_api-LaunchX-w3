bab7422b3ba90a729c9168d8efedd996df3a6692
Marco <macdev.gmail@gmail.com>
2022-04-30 20:42:13 -0500

    Create a DELETE method which deletes an explorer by id

--- app.js
+++ app.js
@@ -41,3 +41,9 @@ app.put('/v1/explorers/:id', (req, res) => {
   const requestBody = req.body
   res.status(200).json({message: "Updated"})
 })
+app.delete('/v1/explorers/:id', (req, res) => {
+  console.log(`Api Explorers DELETE request ${new Date()}`)
+  console.log (`Delete explorer with id ${req.params.id}`)
+  const requestBody = req.body
+  res.status(200).json({message: "Deleted"})
+})
