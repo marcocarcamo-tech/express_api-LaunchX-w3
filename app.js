be4098d013331d8a978282bdfeb3ccbc6b5d6a0a
Marco <macdev.gmail@gmail.com>
2022-04-30 20:29:23 -0500

    added an endpoint which returns a explorer with an specified ID

--- app.js
+++ app.js
@@ -22,3 +22,11 @@ app.get('/v1/explorers', (req, res) => {
   const explorers = [explorer1, explorer2, explorer3, explorer4]
   res.status(200).json(explorers)
 })
+
+
+app.get('/v1/explorers/:id', (req, res) => {
+  console.log(`Api Explorers GET request ${new Date()}`)
+  console.log(`Getting explorer with id ${req.params.id}`)
+  const explorer = {id: 1,  name: "Marco"}
+  res.status(200).json(explorer)
+})
